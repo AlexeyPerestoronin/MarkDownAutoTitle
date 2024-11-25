63889063a759194d80741ba26aca30427648e473
Aleksey Perestoronin <Aleksey.Perestoronin@myoffice.team>
2024-11-25 23:49:03 +0600

    temporary-1

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use clap::Parser;
+use std::{fs::OpenOptions, path::Path};
 
 #[derive(Parser, Debug)]
 #[command(
-    version="1.0.0.0",
-    about="Command Line Tool for auto-adding title to mark-down file")
-]
+    version = "1.0.0.0",
+    about = "Command Line Tool for auto-adding title to mark-down file"
+)]
 struct Args {
-    /// target mark-down file for title addition
+    /// Target mark-down file for title addition
     #[arg(short, long)]
     file_name: String,
+
+    /// Title message for title addition
+    #[arg(long, default_value = "Auto-Title:")]
+    title_message: String,
+
+    /// Whether to add links to navigation between titles
+    #[arg(long, default_value_t = false)]
+    with_links: bool,
 }
 
 fn main() {
     let arguments = Args::parse();
+
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(false)
+        .open(Path::new(&arguments.file_name))
+        .unwrap();
 
     // println!("CLI count is {}", arguments.count);
     println!("CLI file-name is {}", arguments.file_name);
---
+++ test_files/simple/expected.md
@@ -0,0 +1,17 @@
+# Auto-Title:
+1. Header-1.1: description
+2. Header-1.2: description
+3. Header-1.3: description
+4. Header-1.4: description
+
+# Header-1.1: description
+Content...
+
+# Header-1.2: description
+Content...
+
+# Header-1.3: description
+Content...
+
+# Header-1.4: description
+Content...
