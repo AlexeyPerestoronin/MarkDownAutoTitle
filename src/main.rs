6237e06e84d3ee1b9a5b0e478f0fed03fe76ff8c
Aleksey Perestoronin <Aleksey.Perestoronin@myoffice.team>
2024-11-26 11:58:26 +0600

    temporary-2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use clap::Parser;
-use std::{fs::OpenOptions, path::Path};
+use regex::Regex;
+use std::{
+    fs::OpenOptions,
+    io::{BufRead, BufReader},
+    path::Path,
+};
 
 #[derive(Parser, Debug)]
 #[command(
@@ -18,18 +23,54 @@ struct Args {
     /// Whether to add links to navigation between titles
     #[arg(long, default_value_t = false)]
     with_links: bool,
+
+    /// Whether to print information about process in console
+    #[arg(long, default_value_t = false)]
+    verbose: bool,
 }
 
 fn main() {
     let arguments = Args::parse();
 
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(false)
-        .open(Path::new(&arguments.file_name))
-        .unwrap();
+    let titles_info = detect_titles(Path::new(&arguments.file_name));
+    for title_info in &titles_info {
+        println!("line number: {}", title_info.line_number);
+        println!("title: {}", title_info.title);
+        println!("level: {}", title_info.level);
+    }
 
     // println!("CLI count is {}", arguments.count);
     println!("CLI file-name is {}", arguments.file_name);
+}
+
+struct TitleInfo {
+    line_number: usize,
+    title: String,
+    level: usize,
+}
+
+fn detect_titles(file_path: &Path) -> Vec<TitleInfo> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(false)
+        .open(file_path)
+        .unwrap();
+
+    let reader = BufReader::new(file);
+    let reg_title_pattern = Regex::new(r"(?<level>#+)\s+(?<title>.+)").unwrap();
+    let mut titles_info: Vec<TitleInfo> = vec![];
+    for (line_number, line) in reader.lines().enumerate() {
+        if let Ok(line) = line {
+            if let Some(capture) = reg_title_pattern.captures(line.as_str()) {
+                titles_info.push(TitleInfo {
+                    line_number: line_number,
+                    title: String::from(&capture["title"]),
+                    level: *&capture["level"].chars().filter(|ch| *ch == '#').count(),
+                });
+            }
+        }
+    }
+
+    return titles_info;
 }
